d9e9377d8ccea3e4212ade4d10edf8e17b3adcc1
JLMengsk <jlmengsk@gmail.com>
2020-06-01 23:45:50 +0800

    Add Array

---
+++ Array/Max_Consecutive_Ones.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+# @Time     : 6/1/2020 11:42 PM
+# @Author   : Jupiter
+# @Site     : 
+# @File     : Max_Consecutive_Ones.py
+# @Software : PyCharm
+
+Class Solution:
---
+++ Array/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding:utf-8 -*-
+# @Time     : 6/1/2020 11:40 PM
+# @Author   : Jupiter
+# @Site     : 
+# @File     : __init__.py.py
+# @Software : PyCharm
