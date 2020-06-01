815e3868ba25b7faa98595fd939403cbd6fef348
JLMengsk <jlmengsk@gmail.com>
2020-06-02 00:16:48 +0800

    Add soultion2

--- Array/Max_Consecutive_Ones.py
+++ Array/Max_Consecutive_Ones.py
@@ -5,7 +5,7 @@
 # @File     : Max_Consecutive_Ones.py
 # @Software : PyCharm
 
-class Solution:
+class Solution1:
     def findMaxConsecutiveOnes(self, nums):
         """
            :type nums: List[int]
@@ -18,12 +18,22 @@ class Solution:
             # if a == nums[i]:  # This a is useless
             if nums[i] == 1:
                 count += 1
-                if count > max:
-                    max = count  # need to compare with the max number
+                max = max if max > count else count # COOL METHOD
+                # if count > max:
+                #     max = count  # need to compare with the max number
             else:
                 count = 0
         return max  # find the maximum number of consecutive 1s in this array
 
 
-s = Solution()  # Instantiated
+s = Solution1()  # Instantiated
 print(s.findMaxConsecutiveOnes([0, 1, 1, 1, 0, 1, 1]))
+
+
+class Solution2:
+    def findMaxConsecutiveOnes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return len(max("".join(map(str, nums)).split("0")))
