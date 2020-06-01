6a69d42a652c5ff650252039debaa3b8cb32c71f
JLMengsk <jlmengsk@gmail.com>
2020-06-02 00:00:37 +0800

    Question 1

--- Array/Max_Consecutive_Ones.py
+++ Array/Max_Consecutive_Ones.py
@@ -5,4 +5,25 @@
 # @File     : Max_Consecutive_Ones.py
 # @Software : PyCharm
 
-Class Solution:
+class Solution:
+    def findMaxConsecutiveOnes(self, nums):
+        """
+           :type nums: List[int]
+           :rtype: int
+        """
+        # a = 1  In this question, there are just 1s and 0s
+        max = 0
+        count = 0
+        for i in range(0, len(nums)):  # RANGE(0,6) ---- 0 1 2 3 4 5
+            # if a == nums[i]:  # This a is useless
+            if nums[i] == 1:
+                count += 1
+                if count > max:
+                    max = count  # need to compare with the max number
+            else:
+                count = 0
+        return max  # find the maximum number of consecutive 1s in this array
+
+
+s = Solution()  # Instantiated
+print(s.findMaxConsecutiveOnes([0, 1, 1, 1, 0, 1, 1]))
