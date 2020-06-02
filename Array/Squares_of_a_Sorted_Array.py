8c1a8213e5cad80ba633385b32ab44520cc878ab
JLMengsk <jlmengsk@gmail.com>
2020-06-02 13:46:05 +0800

    question 3 s1 06/02/2020

---
+++ Array/Squares_of_a_Sorted_Array.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+# @Time     : 6/2/2020 12:20 PM
+# @Author   : Jupiter
+# @Site     : 
+# @File     : Squares_of_a_Sorted_Array.py
+# @Software : PyCharm
+
+# Given an array of integers A sorted in non-decreasing order, return an array of the squares of each number, also in sorted non-decreasing order.
+
+class Solution1:
+    def sortedSquares(self, A):
+        for i in range(len(A)):
+            A[i] = (A[i] ** 2)
+            # A.sort() Wrong Position
+        A.sort()  # Pay attention where to use sort. Finished the whole loop then sort
+        return A
+
+
+s = Solution1()
+print(s.sortedSquares([-4, -1, 0, 3, 10]))
