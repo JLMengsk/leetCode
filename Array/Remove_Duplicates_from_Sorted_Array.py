d88fd58108d0a83768497f9d8915feb405b38237
JLMengsk <jlmengsk@gmail.com>
2020-06-03 20:56:49 +0800

    Need more research on way 2

--- Array/Remove_Duplicates_from_Sorted_Array.py
+++ Array/Remove_Duplicates_from_Sorted_Array.py
@@ -8,12 +8,30 @@
 # Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
 # Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
 
-class Solution:
+class Solution1:
     def removeDuplicates(self, nums):
-        nums = list(set(nums))
-        return len(nums)  # I don't know why it can't work in leetcode
+        nums[:] = sorted(list(set(nums)))
+        return nums
+        # nums = list(set(nums))
+        # return len(nums)  # it can't work in leetcode
+
+
 # nums[:]=sorted(list(set(nums)))  This one works
 
+class Solution2:
+    # @return an integer
+    def removeDuplicates(self, nums):
+        if len(nums) == 0:
+            return 0
+        j = 0
+        for i in range(0, len(nums)):
+            if nums[i] != nums[j]:
+                nums[i], nums[j + 1] = nums[j + 1], nums[i] # no matter how much is nums[i]   Just get j
+                j = j + 1
+        return j + 1
 
-s = Solution()
+
+s = Solution1()
+print(s.removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]))
+s = Solution2()
 print(s.removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]))
