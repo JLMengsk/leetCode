2dea06e661faeeeed56de2667a24a187cd34c61f
JLMengsk <jlmengsk@gmail.com>
2020-06-07 19:56:03 +0800

    question 3 06072020

--- String/Plus_One.py
+++ String/Plus_One.py
@@ -8,9 +8,41 @@
 # Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
 # The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
 
-# class Solution:
-#     def plusOne(self, digits):
-#         return int("".join([str(s) for s in digits]))+1
+class Solution1:
+    def plusOne(self, digits):
+        return list(map(int, [char for char in str(int("".join([str(s) for s in digits])) + 1)]))
+# Very important way
+# results = list(map(str,digits))
 
-s = Solution()
-print(s.plusOne([1,2,3]))
+# Change Array to numbers
+# results="".join(results)
+
+# results = str(int(results)+1)
+
+# Changer numbers to array
+# results = [char for char in results]
+
+# results = list(map(int,results))
+# return results
+
+# Set flag to 1 but slow
+class Solution2:
+    def plusOne(self, digits):
+        flag = 1
+        for i in range((len(digits)) - 1, -1, -1):
+            if digits[i] + flag == 10:
+                digits[i] = 0
+                flag = 1
+            else:
+                digits[i] += flag
+                flag = 0
+
+        if flag == 1:
+            digits.insert(0, 1)
+        return digits
+
+
+s = Solution1()
+print(s.plusOne([1, 2, 3]))
+s = Solution2()
+print(s.plusOne([1, 2, 3]))
