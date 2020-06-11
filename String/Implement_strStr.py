3cff88358934307722b778ec779488d57ebc37d3
JLMengsk <jlmengsk@gmail.com>
2020-06-12 00:13:27 +0800

    question su2 7

--- String/Implement_strStr.py
+++ String/Implement_strStr.py
@@ -7,13 +7,24 @@
 
 # Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
 
-class Solution:
+class Solution1:
     def strStr(self, haystack, needle):
+        if needle == "":
+            return 0
         if needle in haystack:
             return haystack.index(needle)
         else:
             return -1
 
+#Faster
+class Solution2:
+    def strStr(self, haystack, needle):
+        if len(needle) == 0:
+            return 0
+        return haystack.find(needle)
 
-s = Solution()
+
+s = Solution1()
+print(s.strStr("hello", "ll"))
+s = Solution2()
 print(s.strStr("hello", "ll"))
