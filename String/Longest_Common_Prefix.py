041904d6327701a2e5c28a84713875e2bdf0ae96
JLMengsk <jlmengsk@gmail.com>
2020-06-12 20:41:39 +0800

    question 8 su3 06122020

--- String/Longest_Common_Prefix.py
+++ String/Longest_Common_Prefix.py
@@ -36,6 +36,15 @@ class Solution2:
         return pref
 
 
+# >>>a = [1,2,3]
+# >>> b = [4,5,6]
+# >>> c = [4,5,6,7,8]
+# >>> zipped = zip(a,b)     # 打包为元组的列表
+# [(1, 4), (2, 5), (3, 6)]
+# >>> zip(a,c)              # 元素个数与最短的列表一致
+# [(1, 4), (2, 5), (3, 6)]
+# >>> zip(*zipped)          # 与 zip 相反，*zipped 可理解为解压，返回二维矩阵式
+# [(1, 2, 3), (4, 5, 6)]
 class Solution3:
     def longestCommonPrefix(self, strs):
         if not strs:
