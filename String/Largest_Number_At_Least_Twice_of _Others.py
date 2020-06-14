685c4a5d46e8bee043ddc3996271c884ce42edb5
JLMengsk <jlmengsk@gmail.com>
2020-06-14 13:33:35 +0800

    Review add su 4

--- String/Largest_Number_At_Least_Twice_of _Others.py
+++ String/Largest_Number_At_Least_Twice_of _Others.py
@@ -17,15 +17,16 @@ class Solution1:
         a = copy.copy(nums)
         m1 = max(a)
         a.remove(m1)
-        m2 = max(a)
+        m2 = max(a)  # Second bigger
         if m1 >= 2 * m2:
             return nums.index(m1)
         return -1
 
 
+# Quick way
 class Solution2:
     def dominantIndex(self, nums):
-        if len(nums) < 2:
+        if len(nums) < 2:  # only one element is 1
             return 0
         else:
             sort = sorted(nums)
@@ -34,6 +35,7 @@ class Solution2:
         return -1
 
 
+# Scan All without python method
 class Solution3:
     def dominantIndex(self, nums):
         maxIndex = 0
@@ -47,9 +49,30 @@ class Solution3:
         return maxIndex
 
 
+# Linear Scan
+class Solution4(object):
+    def dominantIndex(self, nums):
+        m = max(nums)
+        if all(m >= 2 * x for x in nums if x != m):
+            return nums.index(m)
+        return -1
+        """
+        all() 函数用于判断给定的可迭代参数 iterable 中的所有元素是否都为 TRUE，如果是返回 True，否则返回 False。
+        元素除了是 0、空、None、False 外都算 True。
+        函数等价于：
+        def all(iterable):
+            for element in iterable:
+                if not element:
+                    return False
+            return True
+        """
+
+
 s = Solution1()
 print(s.dominantIndex([1, 2, 3, 4]))
 s = Solution2()
 print(s.dominantIndex([1, 2, 3, 4]))
 s = Solution3()
+print(s.dominantIndex([1, 2, 3, 4]))
+s = Solution4()
 print(s.dominantIndex([1, 2, 3, 4]))
