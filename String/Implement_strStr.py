50d4a2a5d1afd56b0f398caf3f6992a00d14cd17
JLMengsk <jlmengsk@gmail.com>
2020-06-15 11:43:57 +0800

    Review add su 3

--- String/Implement_strStr.py
+++ String/Implement_strStr.py
@@ -23,8 +23,22 @@ class Solution2:
             return 0
         return haystack.find(needle)
 
+# 采用brute force方式，即依次从字符串的每个位置开始，截取和子字符串相同长度的字符串，与给定的子字符串进行比较。
+class Solution3:
+    def strStr(self, haystack, needle):
+        if needle == "":
+            return 0
+        else:
+            for i in range(len(haystack) - len(needle)+1):
+                # haystack: florida
+                # needle:        da
+                if haystack[i:i+len(needle)] == needle:
+                    return i
+            return -1
 
 s = Solution1()
 print(s.strStr("hello", "ll"))
 s = Solution2()
+print(s.strStr("hello", "ll"))
+s = Solution3()
 print(s.strStr("hello", "ll"))
