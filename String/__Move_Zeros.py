0c2fec682558ae6b8ca8d4cb05fb46ac15ccc5c0
JLMengsk <jlmengsk@gmail.com>
2020-06-19 21:11:35 +0800

    HAHA

---
+++ String/__Move_Zeros.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+# @Time     : 6/19/2020 9:11 PM
+# @Author   : Jupiter
+# @Site     : 
+# @File     : __Move_Zeros.py
+# @Software : PyCharm
+
+# Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+s = Solution1()
+print(s)
