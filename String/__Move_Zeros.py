1ced409d888977426acf47405fae205f844eaffc
JLMengsk <jlmengsk@gmail.com>
2020-06-20 17:29:40 +0800

    Failed to complete them

--- String/__Move_Zeros.py
+++ String/__Move_Zeros.py
@@ -7,5 +7,24 @@
 
 # Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 
+# class wrong_Solution:
+#     def moveZeroes(self, nums):
+#         for i in nums:
+#             if i == 0:
+#                 nums.append(nums.pop(i))
+#         return nums
+
+class Solution1:
+    def moveZeroes(self, nums):
+        i = 0
+        for j in nums:
+            if j != 0:
+                nums[i] = j
+                i += 1
+        while i < len(nums):
+            nums[i] = 0
+            i += 1
+        return nums
+
 s = Solution1()
-print(s)
+print(s.moveZeroes([0,1,0,3,12]))
--- String/__Remove_Duplicates_from_Sorted_Array.py
+++ String/__Remove_Duplicates_from_Sorted_Array.py
@@ -8,6 +8,17 @@
 # Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
 # Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
 
+class Solution1:
+    def removeDuplicates(self, nums):
+        if len(nums) == 0:
+            return 0
+        cur = 0
+        for i in range(1, len(nums)):
+            if nums[i] != nums[cur]:
+                cur += 1
+                nums[cur] = nums[i]
+        return cur + 1
+
 
 s = Solution1()
-print(s)
+print(s.removeDuplicates([1, 1, 2]))
