384a0b7940e4d00969fd34ba451063bab8f7541d
JLMengsk <jlmengsk@gmail.com>
2020-06-25 21:05:58 +0800

    Hard One

---
+++ Stack/_Target_Sum.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+# @Time     : 6/25/2020 8:59 PM
+# @Author   : Jupiter
+# @Site     : 
+# @File     : _Target_Sum.py
+# @Software : PyCharm
+
+# You are given a list of non-negative integers, a1, a2, ..., an, and a target, S.
+# Now you have 2 symbols + and -.
+# For each integer, you should choose one from + and - as its new symbol.
+
+class Solution:
+    def findTargetSumWays(self, nums: List[int], S: int) -> int:
+
+
+s = Solution1()
+print(s)
