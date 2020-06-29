828f7c3090ff8a941ce8d47b4e58cb4488c0bf49
JLMengsk <jlmengsk@gmail.com>
2020-06-29 21:14:47 +0800

    New

---
+++ Queue and Stack/Decode_String.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+# @Time     : 6/29/2020 9:11 PM
+# @Author   : Jupiter
+# @Site     : 
+# @File     : Decode_String.py
+# @Software : PyCharm
+
+# Given an encoded string, return its decoded string.
+# The encoding rule is: k[encoded_string],
+# where the encoded_string inside the square brackets is being repeated exactly k times.
+# Note that k is guaranteed to be a positive integer.
+
+s = Solution1()
+print(s)
